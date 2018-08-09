42cbc53432e2aa9a6026b6bc14dd323e023eea92
Txivo Juarez Lap <txivojuarezlap@MacBook-Pro-de-Txivo.local>
2018-08-09 18:46:07 -0500

    merged versions

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,13 @@
 // Gobal veriables
 // ==========================================================================
-
-
-
-
 var wins = 0;
- var loses = 0;
- 
-
+var loses = 0;
 // Created array of words
 var word = ["javascript", "red", "sterbend", "pink", "police"];
 
 // Choose word randomly
 var randNum = Math.floor(Math.random()*word.length);
-var wordSelected = word [randNum];
+var wordSelected = word[randNum];
 var rGuess = [];
 var wGuess = [];
 var emptyArray = []; 
@@ -23,8 +17,7 @@ console.log(wordSelected);
 
 var docUnder = document.getElementsByClassName('emptyArray');
 var docRguess = document.getElementsByClassName('rGuess');
-var docWGuess = document.getElementsByClassName('wGuess');
-
+var docWguess = document.getElementById('wGuess');
 
 // Main
 // ==========================================================================
@@ -34,60 +27,60 @@ var underScore = () => {
 
     for  (var i =0 ; i < wordSelected.length; i ++){
         emptyArray.push('_');
-        
     }
-
+    docWguess.innerHTML = emptyArray.length;
+    console.log(emptyArray.length);
     return emptyArray;
-}
+};
 
 console.log(underScore());
 
-// reduce number of guesses left
-// var guessLeft = () => {
-
-//     for  (var i =0 ; i < wordSelected.length; i --){
-//         wGuess.push(i);
-//     }
-
-//     return wGuess;
-// }
-
-// console.log(guessLeft());
-
-
-
 // user guesses
 document.addEventListener('keypress', (event) => {
-    var keyword = string.fromcharcode(event.keycode);
-    
-// if users guess is right
-    if (wordSelected.indexOf.(keyword) > -1){
-        console.log (true); 
-
+    var keyword = String.fromCharCode(event.keyCode);
+    // if users guess is right
+    if (wordSelected.indexOf(keyword) > -1){
         // add to right guess array
-        rGuess.push(keyword);
-        console.log (rGuess); 
-
         // replace underscore with right letter
         emptyArray[wordSelected.indexOf(keyword)] = keyword;
         docUnder[0].innerHTML = emptyArray.join('');
-        docRguess[0].innerHTML = rGuess.join('');
-        
-
+        rGuess = emptyArray;
         // check if user word matches guess
         if (emptyArray.join('') == wordSelected){
-
             alert('You win!')
         }        
+    } else {
+        result = guessLeft();
+        console.log(result);
+        if (result == false)
+            alert("You Lose");
+        //wGuess.push(keyword);
+        //docWguess[0].innerHTML = wGuess.join('');
     }
-    
-    else {
 
-        wGuess.push(keyword);
-        console.log (wGuess); 
-        docWguess[0].innerHTML =wGuess.join('');
-    }
-
-    docUnder[0].innerHTML = underScore.join('');
+    docUnder[0].innerHTML = emptyArray.join('');
 });
 
+// reduce number of guesses left
+ var guessLeft = () => {
+
+    var guessesleft = docWguess.innerHTML;
+    console.log(guessesleft);
+    
+    if (guessesleft >0)
+    {
+        guessesleft = guessesleft -1;
+        console.log(guessesleft);
+        docWguess.innerHTML= guessesleft;   
+        return true;
+    }
+
+    if (guessesleft ==0)
+    {
+        guessesleft = guessesleft -1;
+        docWguess.innerHTML = guessesleft;   
+        return false;
+    }
+ }
+
+// console.log(guessLeft());
